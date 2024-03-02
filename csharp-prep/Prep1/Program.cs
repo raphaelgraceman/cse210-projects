bca85c5e36fa8e33bb734c1693e9481d1b1b1f5a
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-03-02 15:42:40 +0000

    Finished prep 1 asssignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        //prep 1 assignement 
+       //request user fname
+       Console.WriteLine("What is your fname?");
+       string fname = Console.ReadLine();
+
+       //get the second name
+       Console.WriteLine("What is your last name?");
+       string lname = Console.ReadLine();
+
+       //Print user full name
+       Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
