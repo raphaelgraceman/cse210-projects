2fcf065b5081983d9c57b90198099153cc7084a4
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-03-03 02:27:56 +0000

    copleted prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -10,12 +10,16 @@ class Program
         int magicNumber =  randomNumber.Next(1, 100);
 
         int guess = -1;
+        int guessCount = 0;
 
         //looping the user guess
         while (guess != magicNumber)
         {
-            Console.WriteLine("Guess a magic number.");
+           if (guessCount != 5)
+           {
+             Console.WriteLine("Guess a magic number.");
             guess = int.Parse(Console.ReadLine());
+            guessCount++ ;
 
             if (magicNumber > guess)
             {
@@ -29,6 +33,37 @@ class Program
             {
                 Console.WriteLine("You guessed it!");
             }
+           }
+
+           else
+           {
+            Console.WriteLine("Game Over");
+            string  response = "yes";
+            if (response == "yes")
+            {
+                Console.WriteLine("Would you like to continue?");
+                response = Console.ReadLine();
+            }
+
+            //continueing
+             Console.WriteLine("Guess a magic number.");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++ ;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Guess higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Guess lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            Console.WriteLine("Game over");
+           }
         }
     }
 }
