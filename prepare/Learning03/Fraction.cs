7f74d378ec8f0c0d3f8507fb6f205dfd8be65dfe
nuturethemyoungwell <raphaelprominence10@gmail.com>
2024-03-20 13:09:02 +0000

    Reviewed learning material

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -7,25 +7,29 @@ public class Fraction
     private int _top;
     private int _bottom;
 
+    //Calling the special constructor
     public Fraction()
     {
-        // Default to 1/1
+        // constructor with no parameter and initializes the no. to 1/1
         _top = 1;
         _bottom = 1;
     }
 
+    //Special constructor with one parameter call wholeNumber
     public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1;
     }
 
+    //A constructor with 2 parameters
     public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
     }
 
+    //Create Getters
     public string GetFractionString()
     {
         string text = $"{_top}/{_bottom}";
